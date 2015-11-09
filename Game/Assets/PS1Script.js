938b80d315e31c5f317fd6aad1b7beeeead65b45
Alexis <arschlag@gmail.com>
2015-11-09 16:52:02 -0700

    Better BoatMove PS

--- Game/Assets/PS1Script.js
+++ Game/Assets/PS1Script.js
@@ -1,12 +1,22 @@
 ﻿#pragma strict
 
 var player : GameObject;
-
+var PS : GameObject;
+var rb = player.GetComponent.<Rigidbody2D>();
 function Start () {
-	var rb = player.GetComponent.<Rigidbody2D>();
+	
 
 }
 
 function Update () {
+	transform.position.y = player.transform.position.y;
+	transform.position.x = player.transform.position.x;
+	
+	transform.rotation = player.transform.rotation;
+	
+	//changes based on movement of boat
+	
 
+	PS.GetComponent.<ParticleSystem>().emissionRate = rb.velocity.magnitude * 5;
+	PS.GetComponent.<ParticleSystem>().startSpeed = rb.velocity.magnitude * 3;
 }
