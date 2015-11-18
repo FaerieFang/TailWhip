44a94ef3653736bc86435bc6a965514120e5e6d3
PrincessFaerie <arschlag@gmail.com>
2015-11-18 14:47:30 -0700

    Background updates (still needs work)

--- Game/Assets/Scripts/Control/backgroundScript.js
+++ Game/Assets/Scripts/Control/backgroundScript.js
@@ -6,13 +6,13 @@ var controler : GameObject;
 
 function Start () {
 	many = controler.GetComponent(ControlScript).backCount;
-	if (many < 100){
+	if (many < 1000){
 		var clone : GameObject;
 		
 		clone = Instantiate(object, new Vector2 (transform.position.x + .96, transform.position.y), transform.rotation);
 		clone = Instantiate(object, new Vector2 (transform.position.x - .96, transform.position.y), transform.rotation);
-		clone = Instantiate(object, new Vector2 (transform.position.x, transform.position.y + 1.40), transform.rotation);
-		clone = Instantiate(object, new Vector2 (transform.position.x, transform.position.y - 1.40), transform.rotation);
+		clone = Instantiate(object, new Vector2 (transform.position.x, transform.position.y + 1.4), transform.rotation);
+		clone = Instantiate(object, new Vector2 (transform.position.x, transform.position.y - 1.4), transform.rotation);
 		controler.GetComponent(ControlScript).backCount += 1;
 	}
 }
@@ -20,3 +20,11 @@ function Start () {
 function Update () {
 
 }
+/*
+function OnTriggernEnter2D (coll: Collider2D){
+	if (coll.gameObject.tag == "background"){
+		Debug.Log("test");
+		Destroy (coll.gameObject);
+	}
+}
+*/
